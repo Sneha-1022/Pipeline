b17236b9f9f93609244d97d58aa14d9bb91c4095
Sneha-1022 <sneha.kutti1022@gmail.com>
2024-07-16 12:50:57 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,1 +1,14 @@
+import logging
+import json
+
+# Configure logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    logger.info("Lambda function invoked")
+    return {
+        'statusCode': 200,
+        'body': json.dumps("Hello, World!")
+    }
 
