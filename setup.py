6535b293bc910a001fe53c39fea22367baea81be
Sneha-1022 <sneha.kutti1022@gmail.com>
2024-07-16 11:27:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,18 @@
 
+### `setup.py`
+
+Optionally, if you want to use a `setup.py` file to manage your Python package, you can include one:
+
+```python
+from setuptools import setup, find_packages
+
+setup(
+    name='lambda_function',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'boto3',
+        'requests'
+    ],
+)
+
